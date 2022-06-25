d67a9af69fa2583fb7f785e8690a741125f1f8fc
abada21 <badatunde58@gmail.com>
2022-06-25 13:05:06 +0100

    done

---
+++ index.js
@@ -0,0 +1,8 @@
+
+var myName = "Bada Abdulrasaq";
+var height = "1.5 metres";
+var country = "Nigeria";
+alert("My name is " +myName+", I am "+height+ " tall, "+ "I am from "+ country);
+console.log("My name is " +myName+", I am "+height+ " tall, "+ "I am from "+ country);
+
+
